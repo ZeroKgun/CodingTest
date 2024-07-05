fa01b2fd8fb854948c352d5d55edcab017c341cd
ZeroKgun <102206256+ZeroKgun@users.noreply.github.com>
2024-07-05 11:09:50 +0900

    [level 2] Title: 올바른 괄호, Time: 19.12 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,7 +4,7 @@ class Solution {
     boolean solution(String s) {
         boolean answer = true;
         
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> stack = new ArrayDeque<>();
         for(int i=0; i<s.length(); i++){
             if(stack.isEmpty()){
                 stack.push(s.charAt(i));
