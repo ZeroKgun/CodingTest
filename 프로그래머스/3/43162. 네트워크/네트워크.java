c24d0472439f7704040a5edd8c385365fa7520af
ZeroKgun <102206256+ZeroKgun@users.noreply.github.com>
2024-07-19 13:20:53 +0900

    [level 3] Title: 네트워크, Time: 0.72 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,40 @@
+import java.util.*;
+
 class Solution {
+    static Map<Integer, List<Integer>> network = new HashMap<>();
+    static boolean[] visited;
     
-    int n;
-    int[][] computers;
-    boolean[] visited;
-    int[][] network;
-    
-    public void dfs(int idx){
-        visited[idx] = true;
-        for(int i =0; i<n; i++){
-            if(idx != i){
-                if(visited[i] == false && network[idx][i] == 1){
-                    dfs(i);   
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[n];
+        for(int i = 0; i<computers.length; i++){
+            network.put(i, new ArrayList<>());
+        }
+        for(int i = 0; i<computers.length ; i++){
+            for(int j=0; j<computers[i].length ; j++){
+                if(i==j) continue;
+                if(computers[i][j] == 1){
+                    network.get(i).add(j);
                 }
             }
         }
-    }
-    
-    public int solution(int n, int[][] computers) {
-        
-        int answer = 0;
-        this.n = n;
-        this.network = computers;
-        visited = new boolean[computers[0].length];
-        
-        for(int i = 0; i<n ; i++){
+        for(int i = 0; i<visited.length; i++){
             if(visited[i] == false){
                 dfs(i);
                 answer++;
             }
         }
+        
         return answer;
+    }
+    
+    public static void dfs(int start){
+        visited[start] = true;
+        
+        for(Integer next : network.get(start)){
+            if(!visited[next]){
+                dfs(next);
+            }
+        }
     }
 }
