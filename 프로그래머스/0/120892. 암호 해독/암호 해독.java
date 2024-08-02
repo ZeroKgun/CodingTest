c8fd0698664e055d1f32e79620c65d4f1e484280
ZeroKgun <102206256+ZeroKgun@users.noreply.github.com>
2024-08-02 14:48:28 +0900

    [level 0] Title: 암호 해독, Time: 15.12 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        
+        
+        
+        for(int i=1; code*i<=cipher.length(); i++){
+            answer += cipher.charAt(i*code-1);
+        }
+        return answer;
+    }
+}
