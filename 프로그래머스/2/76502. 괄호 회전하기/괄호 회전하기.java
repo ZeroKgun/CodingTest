9ad642b4396061e01a0dd1602406e5f0adda3793
ZeroKgun <102206256+ZeroKgun@users.noreply.github.com>
2024-10-21 13:22:56 +0900

    [level 2] Title: 괄호 회전하기, Time: 83.91 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,48 +1,45 @@
 import java.util.*;
 
+
 class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        if(isOk(s)) answer++;
+        
+        
+        for(int i = 1; i<s.length(); i++){
+            s = s.substring(1,s.length()) + s.substring(0,1);
+            // System.out.println(s);
+            if(isOk(s)) answer++;
+        }
+        return answer;
+    }
+    
     public boolean isOk(String s){
         Stack<Character> stack = new Stack<>();
-        for(int i=0; i<s.length();i++){
-            if(stack.isEmpty()){
-                stack.push(s.charAt(i));
-            }
-            else if(s.charAt(i)==')'){
-                if(stack.peek()=='('){
+        
+        for(int i=0; i<s.length(); i++){
+            if(!stack.isEmpty()){
+                char c = stack.peek();
+                
+                if(c == '[' && s.charAt(i) == ']'){
                     stack.pop();
                 }
-            }
-            else if(s.charAt(i) == '}'){
-                if(stack.peek() == '{'){
+                else if(c=='{' && s.charAt(i) =='}'){
                     stack.pop();
                 }
-            }
-            else if(s.charAt(i) == ']'){
-                if(stack.peek() == '['){
+                else if(c=='(' && s.charAt(i)==')'){
                     stack.pop();
+                }
+                else{
+                    stack.push(s.charAt(i));
                 }
             }
             else{
                 stack.push(s.charAt(i));
             }
         }
-        
         return stack.isEmpty();
-        
-    }
-    
-    
-    
-    
-    public int solution(String s) {
-        int answer = 0;
-        
-        String check = s;
-        for(int i = 0; i<s.length(); i++){
-            check = check.substring(1)+check.substring(0,1);
-            if(isOk(check)) answer++;
-        }
-        
-        return answer;
     }
 }
