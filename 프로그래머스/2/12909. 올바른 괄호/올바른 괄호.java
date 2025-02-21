403bdc421ecfa6e1fc7097e1bf69e462e7f4669d
ZeroKgun <102206256+ZeroKgun@users.noreply.github.com>
2025-02-21 16:23:45 +0900

    [level 2] Title: 올바른 괄호, Time: 21.08 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,22 +3,24 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
+        Deque<Character> deque = new ArrayDeque<>();
         
-        Deque<Character> stack = new ArrayDeque<>();
         for(int i=0; i<s.length(); i++){
-            if(stack.isEmpty()){
-                stack.push(s.charAt(i));
-            }
-            else if(s.charAt(i) == ')'){
-                if(stack.peek() == '('){
-                    stack.pop();
+            char c = s.charAt(i);
+            
+            if(deque.isEmpty()) deque.push(c);
+            else{
+                if(deque.peek() == '(' && c == ')'){
+                    deque.pop();
+                }
+                else{
+                    deque.push(c);
                 }
             }
-            else{
-                stack.push(s.charAt(i));
-            }
-            
         }
-        return stack.isEmpty();
+        
+        return deque.isEmpty();
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        
     }
 }
