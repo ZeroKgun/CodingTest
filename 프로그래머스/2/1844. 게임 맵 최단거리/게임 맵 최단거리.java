c66f1ec0b1fca1156b889843fb4f35d4d07a6c24
ZeroKgun <102206256+ZeroKgun@users.noreply.github.com>
2025-03-11 19:24:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.73 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -5,40 +5,43 @@ class Solution {
     int[] dx = {0,1,0,-1};
     int[] dy = {1,0,-1,0};
     
-    int[][] visited;
     int n,m;
+    boolean[][] visited;
     
     public int solution(int[][] maps) {
         int answer = 0;
-        int n = maps.length;
-        int m = maps[0].length;
         
-        visited = new int[n][m];
-        visited[0][0] = 1;
+        n = maps.length;
+        m = maps[0].length;
         
-        Queue<int []> que = new LinkedList<>();
+        visited = new boolean[n][m];
+        int[][] cntmap = new int[n][m];
         
-        que.offer(new int[]{0,0});
+        Queue<int[]> que = new LinkedList<>();
+        
+        que.add(new int[]{0,0});
+        visited[0][0] = true;
+        cntmap[0][0] = 1;
         
         while(!que.isEmpty()){
             int[] cur = que.poll();
-            
             int curx = cur[0];
             int cury = cur[1];
             
-            for(int i =0;i<4;i++){
+            for(int i=0; i<4; i++){
                 int nx = curx + dx[i];
                 int ny = cury + dy[i];
                 
-                if(nx>=0 && nx<n && ny>=0 && ny<m){
-                    if(visited[nx][ny] == 0 && maps[nx][ny] != 0){
-                        visited[nx][ny] = visited[curx][cury]+1;
-                        que.offer(new int[]{nx,ny});
+                if(nx>=0 && nx<n && ny>=0 && ny<m && maps[nx][ny] != 0){
+                    if(!visited[nx][ny]){
+                        que.add(new int[]{nx,ny});
+                        cntmap[nx][ny] = cntmap[curx][cury] + 1;
+                        visited[nx][ny] = true;
                     }
                 }
             }
         }
-        if(visited[n-1][m-1] > 0) return visited[n-1][m-1];
+        if(cntmap[n-1][m-1] > 0) return cntmap[n-1][m-1];
         else return -1;
     }
 }
