ed3a11348f4bf582efb76db7b3c7ef5d1d3f4c03
ZeroKgun <102206256+ZeroKgun@users.noreply.github.com>
2025-03-24 20:29:21 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+        int coupon = chicken;
+        
+        while(coupon>=10){
+            int service = coupon / 10;
+            answer += service;
+            coupon = coupon % 10 + service;
+            
+        }
+        
+        
+        return answer;
+    }
+}
